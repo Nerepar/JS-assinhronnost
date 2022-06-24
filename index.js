d29b54b272c7893a89cfd804ac31355d743eb1e7
Nerepar <fedorin.denis.1@yandex.ru>
2022-06-24 18:56:41 +0500

    JS-assinhron

---
+++ index.js
@@ -0,0 +1,34 @@
+// async function asyncOperation() {
+//     await asyncOperation2().then(function (result) {
+//         console.log(result);
+//     });
+//     return 1;
+// }
+
+// async function asyncOperation2() {
+//     return setTimeout(function () {
+//         return 2;
+//     }, 1000);
+// }
+//
+// asyncOperation().then(function (result) {
+//     console.log(result);
+// }).catch(function (error) {
+//     console.log(error);
+// }).finally(function () {
+//     console.log('Finish!');
+// });
+
+// function asyncOperation() {
+//     return Promise.resolve (1);
+// }
+//
+// console.log(asyncOperation());
+
+// function asyncOperation() {
+//     return new Promise(resolve => setTimeout ( => () => {
+//
+//     });
+// }
+//
+// console.log(asyncOperation());
